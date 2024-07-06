cbf584c7d9538b10d387433cb09087cf88ccae50
Cal Courtney <calthejuggler@gmail.com>
2024-07-06 16:50:05 +0100

    chore: shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-	return <div className="text-3xl">Hello world</div>;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<Button>Hello</Button>
+		</div>
+	);
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [react()],
+
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
